446518f0fc8821a530cf90594832fa5599bdc279
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-10-09 08:32:22 -0300

    Feat: controllers updated

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     //expose "/" that return "Hello Word"
+    //localhost8080
 
     @GetMapping("/")
     public String sayHello(){
