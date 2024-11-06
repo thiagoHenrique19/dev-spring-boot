5d3c4ce04fe50c4f0ae81b97da85448dbae5a2b4
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-11-06 11:38:13 -0300

    spring mvc

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -17,6 +22,27 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    //need a controller method to read form data and
+    //add data to the model
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName")String theName,
+                                          Model model){
+
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Hey My Friend from v3!" + theName;
+
+        //add message to the model
+        model.addAttribute("message", result);
+
+
         return "helloworld";
     }
 }
