b117a04a2f5d5f0731c421c9ab83f9cb2dacacd7
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-11-08 11:30:05 -0300

    spring mvc

--- validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,7 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,29 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=1, message="is required" )
     private String lastName = "";
+
+    @Min(value=0, message="must be greater than or equal to zero" )
+    @Max(value=10, message="must be less than or equal to 10" )
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
