f23b46b63111952e34851e4619c7cf9744076523
Thiago Henrique de Oliveira <159919965+thiagoHenrique19@users.noreply.github.com>
2024-11-12 11:22:47 -0300

    spring mvc crud

--- 07-spring-boot-spring-mvc-crud-test/02-thymeleaf-demo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud-test/02-thymeleaf-demo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -10,5 +10,5 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code LOL!
 
     //add a method to sort by last name
-    public List<Employee> fidnAllByOrderByLastNameAsc();
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
--- 07-spring-boot-spring-mvc-crud-test/02-thymeleaf-demo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ 07-spring-boot-spring-mvc-crud-test/02-thymeleaf-demo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -20,7 +20,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        return employeeRepository.fidnAllByOrderByLastNameAsc();
+        return employeeRepository.findAllByOrderByLastNameAsc();
     }
 
     @Override
